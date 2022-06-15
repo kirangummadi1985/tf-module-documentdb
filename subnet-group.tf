3be368268adc79790f593f713456fc60d08eb49e
kiran <kirangummadi1985@gmail.com>
2022-06-15 20:26:49 +0530

    documentdb

---
+++ subnet-group.tf
@@ -0,0 +1,9 @@
+resource "aws_docdb_subnet_group" "main" {
+  name       = "${var.COMPONENT}-${var.ENV}"
+  subnet_ids = var.SUBNET_IDS
+
+  tags = {
+    Name = "${var.COMPONENT}-${var.ENV}"
+  }
+}
+
---
+++ vars.tf
@@ -0,0 +1,10 @@
+variable "ENGINE" {}
+variable "ENGINE_VERSION" {}
+variable "ENV" {}
+variable "COMPONENT" {}
+variable "SKIP_FINAL_SNAPSHOT" {}
+variable "FAMILY" {}
+
+variable "VPC_ID" {}
+variable "VPC_CIDR" {}
+variable "SUBNET_IDS" {}
